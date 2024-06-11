b1358c447ec928c423d58f4bc20fcf06167ca17c
gamono <alpatovaroslav3@gmail.com>
2024-06-11 20:37:11 +0300

    New colors for wallpaper

--- home-manager/modules/wms/hyprland.nix
+++ home-manager/modules/wms/hyprland.nix
@@ -39,7 +39,7 @@
         gaps_in = 5;
         gaps_out = 20;
         border_size = 3;
-        "col.active_border" = "rgba(33ccffee) rgba(00ff99ee) 45deg";
+        "col.active_border" = "rgba(dcddd9ee) hex(101013) 45deg";
         "col.inactive_border" = "rgba(595959aa)";
 
         layout = "dwindle";
