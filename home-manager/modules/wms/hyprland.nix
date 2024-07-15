ac27a659cef2a51a0949fb169f8dfb2583dee42d
gamono <alpatovaroslav3@gmail.com>
2024-07-16 00:33:34 +0300

    Return blur and drop shadow

--- home-manager/modules/wms/hyprland.nix
+++ home-manager/modules/wms/hyprland.nix
@@ -49,15 +49,15 @@
         rounding = 10;
 
         blur = {
-          enabled = false;
+          enabled = yes;
           size = 16;
           passes = 2;
           new_optimizations = true;
-	  xray = true;
-	  ignore_opacity = true;
+	        xray = true;
+	        ignore_opacity = true;
         };
 
-        drop_shadow = false;
+        drop_shadow = yes;
         shadow_range = 4;
         shadow_render_power = 3;
         "col.shadow" = "rgba(1a1a1aee)";
