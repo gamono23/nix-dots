352903f90bc5d995c266fb4ba6fe1f72b36a1f18
gamono <alpatovaroslav3@gmail.com>
2024-07-16 16:21:07 +0300

    Fix cava.nix

--- home-manager/modules/cava.nix
+++ home-manager/modules/cava.nix
@@ -1,22 +1,24 @@
 {
-  enable = true;
-
-  settings = {
-    general = {
-      framerate = 60;
-      lower_cutoff_freq = 20;
-      higher_cutoff_freq = 20000;
-    };
-    output.alacritty_sync = 1;
-    color = {
-      gradient_color_1 = "#bf616a";
-      gradient_color_2 = "#d08770";
-      gradient_color_3 = "#ebcb8b";
-      gradient_color_4 = "#a3be8c";
-      gradient_color_5 = "#8fbcbb";
-      gradient_color_6 = "#88c0d0";
-      gradient_color_7 = "#5e81ac";
-      gradient_color_8 = "#b48ead";
+  programs.cava = {
+    enable = true;
+    
+    settings = {
+      general = {
+        framerate = 60;
+        lower_cutoff_freq = 20;
+        higher_cutoff_freq = 20000;
+      };
+      output.alacritty_sync = 1;
+      color = {
+        gradient_color_1 = "#bf616a";
+        gradient_color_2 = "#d08770";
+        gradient_color_3 = "#ebcb8b";
+        gradient_color_4 = "#a3be8c";
+        gradient_color_5 = "#8fbcbb";
+        gradient_color_6 = "#88c0d0";
+        gradient_color_7 = "#5e81ac";
+        gradient_color_8 = "#b48ead";
+      };
     };
   };
 }
