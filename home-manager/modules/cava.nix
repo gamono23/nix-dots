14eec95f4998231ddbb16b48ccd9f351de7f400d
gamono <alpatovaroslav3@gmail.com>
2024-07-16 16:23:10 +0300

    Fix cava.nix

--- home-manager/modules/cava.nix
+++ home-manager/modules/cava.nix
@@ -10,14 +10,14 @@
       };
       output.alacritty_sync = 1;
       color = {
-        gradient_color_1 = "#bf616a";
-        gradient_color_2 = "#d08770";
-        gradient_color_3 = "#ebcb8b";
-        gradient_color_4 = "#a3be8c";
-        gradient_color_5 = "#8fbcbb";
-        gradient_color_6 = "#88c0d0";
-        gradient_color_7 = "#5e81ac";
-        gradient_color_8 = "#b48ead";
+        gradient_color_1 = "'#bf616a'";
+        gradient_color_2 = "'#d08770'";
+        gradient_color_3 = "'#ebcb8b'";
+        gradient_color_4 = "'#a3be8c'";
+        gradient_color_5 = "'#8fbcbb'";
+        gradient_color_6 = "'#88c0d0'";
+        gradient_color_7 = "'#5e81ac'";
+        gradient_color_8 = "'#b48ead'";
       };
     };
   };
