09b13182ddacf8178e9d55657c36a496c3414166
gamono <alpatovaroslav3@gmail.com>
2024-07-16 23:58:53 +0300

    Change driver version (unstable)

--- nixos/modules/nvidia.nix
+++ nixos/modules/nvidia.nix
@@ -18,6 +18,9 @@
       intelBusId = "PCI:0:2:0";
     };
 
+    package = config.boot.kernelPackages.nvidiaPackages.latest;
+ 
+    /* 
     package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
       version = "555.42.02";
       sha256_64bit = "sha256-k7cI3ZDlKp4mT46jMkLaIrc2YUx1lh1wj/J4SVSHWyk=";
@@ -26,5 +29,6 @@
       settingsSha256 = "sha256-rtDxQjClJ+gyrCLvdZlT56YyHQ4sbaL+d5tL4L4VfkA=";
       persistencedSha256 = lib.fakeSha256;
     };
+     */
   }; 
 }
