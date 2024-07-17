1e5cdf46734869b104af4c11bd47ad643f234c04
gamono <alpatovaroslav3@gmail.com>
2024-07-17 14:43:46 +0300

    Add styling to wofi

--- home-manager/modules/wms/wofi.nix
+++ home-manager/modules/wms/wofi.nix
@@ -5,10 +5,28 @@
       * {
         font-family: JetBrains Mono;
       } 
-      
-      window {
+
+      #window {
+        background-color: #434c5e;
         border-radius: 20px;
+      }
+      
+      #outer-box {
+        opacity: 1;
+        border-radius: 20px;
+        color: #eceff4;
+      }
+
+      #input {
+        border-radius: 10px;
         background-color: #4c566a;
+        color: #eceff4;
+      }
+
+      #entry:selected {
+        background-color: #4c566a;
+        border-radius: inherit;
+        border: none;
       }
     '';
   };
